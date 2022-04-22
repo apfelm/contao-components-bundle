797506796cf5f04385dbae60cc1ec4f4607c188f
apfelm <75647571+apfelm@users.noreply.github.com>
2022-04-23 00:18:46 +0200

    Create hello-world frontend module

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+// Frontend modules
+$GLOBALS['TL_LANG']['FMD']['component'] = ['Hello World', 'Gibt den Text "Hello World" aus'];
---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,4 @@
+<?php
+
+// Frontend modules
+$GLOBALS['TL_LANG']['FMD']['apflComponent'] = ['Test-Component', 'Gibt den Text "Hello World" aus'];
